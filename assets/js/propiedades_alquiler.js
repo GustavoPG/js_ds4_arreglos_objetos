46a31f7117bf821aea90cfdafd0508891481bbe1
GustavoPG <gustavoparadag66@gmail.com>
2024-01-10 00:20:40 -0300

    add files

---
+++ assets/js/propiedades_alquiler.js
@@ -0,0 +1,53 @@
+const propiedades_alquiler = [
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: 0,
+        costo: 0,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: 0,
+        costo: 0,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: 0,
+        costo: 0,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: 0,
+        costo: 0,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: 0,
+        costo: 0,
+        smoke: true,
+        pets: true
+    }
+]
+
---
+++ assets/js/propiedades_venta.js
@@ -0,0 +1,52 @@
+const propiedades_ventas = [
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: 0,
+        costo: 0,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: 0,
+        costo: 0,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: 0,
+        costo: 0,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: 0,
+        costo: 0,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: 0,
+        costo: 0,
+        smoke: true,
+        pets: true
+    }
+]
